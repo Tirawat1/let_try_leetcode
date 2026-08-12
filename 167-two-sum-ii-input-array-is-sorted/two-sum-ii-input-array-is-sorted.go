00ef0a53aafff8825b82b9d2f1ad9071457aeed2
Tirawat <98450270+Tirawat1@users.noreply.github.com>
2026-08-12 21:36:09 +0700

    Time: 0 ms (100.00%) | Memory: 7.9 MB (62.57%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.go
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.go
@@ -1,22 +1,17 @@
 func twoSum(numbers []int, target int) []int {
-    ans := make([]int, 2)
 
-	// add logic left and right
-	r := len(numbers) - 1
-	l := 0
+	left, right := 0, len(numbers)-1
+	for left < right {
+		sum := numbers[left] + numbers[right]
 
-	for r >= l {
-		if numbers[l]+numbers[r] == target {
-			ans[0], ans[1] = l+1, r+1
-			break
-		}
-		if numbers[l]+numbers[r] > target {
-			r--
-		} else {
-			l++
+		if sum == target {
+			return []int{left + 1, right + 1}
+		} else if sum > target {
+			right--
+		} else if sum < target {
+			left++
 		}
 
 	}
-
-	return ans
+	return nil
 }
